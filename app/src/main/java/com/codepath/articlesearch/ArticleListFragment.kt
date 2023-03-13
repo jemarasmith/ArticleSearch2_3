44199d3a7300670021abd541a3bddf18e397c17d
Jemara Smith <jsmi8824@students.vsu.edu>
2023-03-12 20:52:42 -0400

    commit 2

--- app/src/main/java/com/codepath/articlesearch/ArticleListFragment.kt
+++ app/src/main/java/com/codepath/articlesearch/ArticleListFragment.kt
@@ -34,7 +34,7 @@ class ArticleListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article_list2, container, false)
+        return inflater.inflate(R.layout.fragment_article_list, container, false)
     }
 
     companion object {
